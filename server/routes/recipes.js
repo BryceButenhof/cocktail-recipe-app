a1738583101492ce369b6b2287c932450b4927f5
jbutenb_jcplc <bryce.thomas.butenhof@jci.com>
2025-05-29 14:12:38 -0500

    Add auth to recipes routes, update tests

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,6 +1,6 @@
 import { RecipeModel } from "../models/recipeModel.js";
-import { UserModel } from "../models/userModel.js";
 import { IngredientModel } from "../models/ingredientModel.js";
+import { AuthMiddleware } from '../middleware/auth.js';
 import express from 'express';
 
 const router = express.Router();
@@ -98,19 +98,12 @@ router.get('/:id', async (req, res) => {
 });
 
 // Create a new recipe
-router.post('/', async (req, res) => {
+router.post('/', AuthMiddleware, async (req, res) => {
     try {
-        // Validate user exists and is not deleted
-        const user = await UserModel.findOne({id: req.body.createdBy});
-        if (!user || user.isDeleted) {
-            return res.status(400).json({ message: `User with id ${req.body.createdBy} was not found` });
-        }
-        
         const ingredientDocuments = await getAndValidateIngredients(req.body.ingredients);
         const formattedIngredients = formatIngredients(ingredientDocuments, req.body.ingredients);
         const abv = calculateABV(ingredientDocuments, req.body.ingredients, req.body.method);
-        const recipe = await new RecipeModel({ ...req.body, createdBy: user._id, ingredients: formattedIngredients, abv}).save();
-
+        const recipe = await new RecipeModel({ ...req.body, createdBy: req.user._id, ingredients: formattedIngredients, abv}).save();
         res.status(201).json(recipe.toRecipeResponse());
     } catch (error) {
         res.status(400).json({ message: error.message });
@@ -118,41 +111,54 @@ router.post('/', async (req, res) => {
 });
 
 // Update a recipe by id
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', AuthMiddleware, async (req, res) => {
     try {
         const recipe = await RecipeModel.findOne({ id: req.params.id, isDeleted: false });
         if (!recipe) {
             return res.status(404).json({ message: `Recipe with id ${req.params.id} was not found` });
         }
 
-        let fieldsToUpdate = {};
+        if (req.user.role !== 'admin' && recipe.createdBy.id !== req.user.id) {
+            return res.status(403).json({ message: 'You do not have permission to update this recipe' });
+        }
 
+        let fieldsToUpdate = {};
         if (req.body.ingredients) {
             const ingredientDocuments = await getAndValidateIngredients(req.body.ingredients);
             fieldsToUpdate.ingredients = formatIngredients(ingredientDocuments, req.body.ingredients);
             fieldsToUpdate.abv = calculateABV(ingredientDocuments, req.body.ingredients, req.body.method);
         }
 
-        const updatedRecipe = await RecipeModel.findOneAndUpdate({ id: req.params.id }, { ...req.body, ...fieldsToUpdate, lastUpdated: Date.now() }, { new: true });
-        res.status(200).json(updatedRecipe.toRecipeResponse());
+        recipe.set({
+            ...req.body,
+            ...fieldsToUpdate,
+            isDeleted: false,
+            lastUpdated: Date.now()
+        });
+        await recipe.save();
+        res.status(200).json(recipe.toRecipeResponse());
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 });
 
 // Delete a recipe
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', AuthMiddleware, async (req, res) => {
     try {
-        const deletedRecipe = await RecipeModel.findOneAndUpdate(
-            { id: req.params.id, isDeleted: false }, 
-            { isDeleted: true }, 
-            { new: true }
-        );
-
-        if (!deletedRecipe) {
+        const recipe = await RecipeModel.findOne({ id: req.params.id, isDeleted: false });
+        if (!recipe) {
             return res.status(404).json({ message: `Recipe with id ${req.params.id} was not found` });
         }
 
+        if (req.user.role !== 'admin' && recipe.createdBy.id !== req.user.id) {
+            return res.status(403).json({ message: 'You do not have permission to delete this recipe' });
+        }
+
+        recipe.set({
+            isDeleted: true,
+            lastUpdated: Date.now()
+        });
+        await recipe.save();
         res.status(200).json({ message: 'Recipe deleted' });
     } catch (error) {
         res.status(400).json({ message: error.message });
--- server/tests/recipes.test.js
+++ server/tests/recipes.test.js
@@ -4,32 +4,91 @@ import app from '../app.js';
 import 'dotenv/config';
 import { IngredientModel } from '../models/ingredientModel.js';
 import { RecipeModel } from '../models/recipeModel.js';
+import { UserModel } from '../models/userModel.js';
+import { v4 as uuid } from 'uuid';
+
+const invalidId = uuid();
+let adminToken,
+    user1Token,
+    user2Token;
+
 
 beforeAll(async () => {
     await mongoose.connect(process.env.ATLAS_TEST_URI);
+
+    //Create users
+    await request(app).post("/users").send({
+        'email': 'admin@admin.com',
+        'username': 'adminUser',
+        'password': 'adminPassword',
+        "role": "admin",
+    });
+
+    await request(app).post("/users").send({
+        'email': 'user1@user.com',
+        'username': 'user1',
+        'password': 'user1Password',
+        "role": "user",
+    });
+
+    await request(app).post("/users").send({
+        'email': 'user2@user.com',
+        'username': 'user2',
+        'password': 'user2Password',
+        "role": "user",
+    });
+
+    //Login users
+    const adminRes = await request(app).post("/users/login").send({
+        'email': 'admin@admin.com',
+        "password": 'adminPassword'
+    });
+    adminToken = adminRes.body.token;
+
+    const user1Res = await request(app).post("/users/login").send({
+        'email': 'user1@user.com',
+        "password": 'user1Password'
+    });
+    user1Token = user1Res.body.token;
+
+    const user2Res = await request(app).post("/users/login").send({
+        'email': 'user2@user.com',
+        "password": 'user2Password'
+    });
+    user2Token = user2Res.body.token;
 });
 
 afterAll(async () => {
+    await UserModel.deleteMany({username: {$in: ['adminUser', 'user1', 'user2']}});
     await RecipeModel.deleteMany({});
     await IngredientModel.deleteMany({});
     await mongoose.connection.close();
 });
 
 describe("Recipe API Tests", () => {
-    let recipeId, honeySyrupId, lemonJuiceId, whiskeyId, bittersId;
+    let recipe1Id, 
+        recipe2Id, 
+        honeySyrupId, 
+        lemonJuiceId, 
+        whiskeyId, 
+        bittersId, 
+        user1Id;
 
     beforeAll(async () => {
+        const user1 = await UserModel.findOne({ username: 'user1' });
+        user1Id = user1._id;
+
         const honeySyrup = await new IngredientModel({
             name: "honey syrup",
             type: "syrup",
-            createdBy: new mongoose.Types.ObjectId('681e4c7c17842c19255e7a2a'),
+            createdBy: user1Id,
         }).save();
         honeySyrupId = honeySyrup.id;
 
         const lemonJuice = await new IngredientModel({
             name: "lemon juice",
             type: "juice",
-            createdBy: new mongoose.Types.ObjectId('681e4c7c17842c19255e7a2a'),
+            createdBy: user1Id,
         }).save();
         lemonJuiceId = lemonJuice.id;
 
@@ -37,20 +96,20 @@ describe("Recipe API Tests", () => {
             name: "whiskey",
             type: "liquor",     
             abv: 40,
-            createdBy: new mongoose.Types.ObjectId('681e4c7c17842c19255e7a2a'),
+            createdBy: user1Id,
         }).save();
         whiskeyId = whiskey.id;
 
         const bitters = await new IngredientModel({
             name: "bitters",
             type: "bitters",
-            createdBy: new mongoose.Types.ObjectId('681e4c7c17842c19255e7a2a'),
+            createdBy: user1Id,
         }).save();
         bittersId = bitters.id;
     });
 
     describe("POST /recipes", () => {
-        it("Should create a recipe successfully", async () => {
+        it("Should not create a recipe without an authorization header", async () => {
             const res = await request(app).post("/recipes").send({
                 name: "Gold Rush",
                 description: "A riff on the classic whiskey sour",
@@ -61,44 +120,65 @@ describe("Recipe API Tests", () => {
                 ],
                 instructions: "Shake with ice and strain into a glass.",
                 method: "shaken",
-                createdBy: "264b106a-7829-4f4a-b286-3a5aee4471e7",
             });
-            expect(res.statusCode).toBe(201);
-            recipeId = res.body.id;
+            expect(res.statusCode).toBe(401);
         });
 
-        it("Should not create a recipe with an invalid user id", async () => {
-            const res = await request(app).post("/recipes").send({
-                name: "Gold Rush",
-                description: "A riff on the classic whiskey sour",
-                ingredients: [
-                    { id: honeySyrupId, quantity: 0.75, unit: "oz" },
-                    { id: lemonJuiceId, quantity: 1, unit: "oz" },
-                    { id: whiskeyId, quantity: 2, unit: "oz" },
-                ],
-                instructions: "Shake with ice and strain into a glass.",
-                method: "shaken",
-                createdBy: "74e3fccc-8dde-470d-8e81-8600e917566b",
-            });
-            expect(res.statusCode).toBe(400);
-            expect(res.body.message).toBe("User with id 74e3fccc-8dde-470d-8e81-8600e917566b was not found");
+        it("Should create a recipe successfully", async () => {
+            const res1 = await request(app).post("/recipes")
+                .send({
+                    name: "Gold Rush",
+                    description: "A riff on the classic whiskey sour",
+                    ingredients: [
+                        { id: honeySyrupId, quantity: 0.75, unit: "oz" },
+                        { id: lemonJuiceId, quantity: 1, unit: "oz" },
+                        { id: whiskeyId, quantity: 2, unit: "oz" },
+                    ],
+                    instructions: "Shake with ice and strain into a glass.",
+                    method: "shaken",
+                })
+                .set({ Authorization: `Bearer ${user1Token}` });
+            expect(res1.statusCode).toBe(201);
+            expect(res1.body.name).toBe("Gold Rush");
+            expect(res1.body.ingredients.length).toBe(3);
+            expect(res1.body.ingredients[0].id).toBe(honeySyrupId);
+            expect(res1.body.ingredients[1].id).toBe(lemonJuiceId);
+            expect(res1.body.ingredients[2].id).toBe(whiskeyId);
+            recipe1Id = res1.body.id;
+
+            const res2 = await request(app).post("/recipes")
+                .send({
+                    name: "Gold Rush",
+                    description: "A riff on the classic whiskey sour",
+                    ingredients: [
+                        { id: honeySyrupId, quantity: 0.75, unit: "oz" },
+                        { id: lemonJuiceId, quantity: 1, unit: "oz" },
+                        { id: whiskeyId, quantity: 2, unit: "oz" },
+                    ],
+                    instructions: "Shake with ice and strain into a glass.",
+                    method: "shaken",
+                })
+                .set({ Authorization: `Bearer ${user2Token}` });
+            expect(res2.statusCode).toBe(201);
+            recipe2Id = res2.body.id;
         });
 
         it("Should not create a recipe with an invalid ingredient id", async () => {
-            const res = await request(app).post("/recipes").send({
-                name: "Gold Rush",
-                description: "A riff on the classic whiskey sour",
-                ingredients: [
-                    { id: honeySyrupId, quantity: 0.75, unit: "oz" },
-                    { id: lemonJuiceId, quantity: 1, unit: "oz" },
-                    { id: "c1e1fce9-f4f2-4cdf-99ad-959e20837789", quantity: 2, unit: "oz" },
-                ],
-                instructions: "Shake with ice and strain into a glass.",
-                method: "shaken",
-                createdBy: "264b106a-7829-4f4a-b286-3a5aee4471e7",
-            });
+            const res = await request(app).post("/recipes")
+                .send({
+                    name: "Gold Rush",
+                    description: "A riff on the classic whiskey sour",
+                    ingredients: [
+                        { id: honeySyrupId, quantity: 0.75, unit: "oz" },
+                        { id: lemonJuiceId, quantity: 1, unit: "oz" },
+                        { id: invalidId, quantity: 2, unit: "oz" },
+                    ],
+                    instructions: "Shake with ice and strain into a glass.",
+                    method: "shaken",
+                })
+                .set({ Authorization: `Bearer ${user1Token}` });
             expect(res.statusCode).toBe(400);
-            expect(res.body.message).toBe("Ingredient with id c1e1fce9-f4f2-4cdf-99ad-959e20837789 was not found");
+            expect(res.body.message).toBe(`Ingredient with id ${invalidId} was not found`);
         });
     });
 
@@ -106,72 +186,125 @@ describe("Recipe API Tests", () => {
         it("Should get all recipes", async () => {
             const res = await request(app).get("/recipes");
             expect(res.statusCode).toBe(200);
-            expect(res.body.length).toBe(1);
+            expect(res.body.length).toBe(2);
         });
     });
 
     describe("GET /recipes/:id", () => {
         it("Should get a recipe by id", async () => {
-            const res = await request(app).get(`/recipes/${recipeId}`);
+            const res = await request(app).get(`/recipes/${recipe1Id}`);
             expect(res.statusCode).toBe(200);
-            expect(res.body.id).toBe(recipeId);
+            expect(res.body.id).toBe(recipe1Id);
         });
 
         it("Should not get a recipe by invalid id", async () => {
-            const res = await request(app).get("/recipes/71077ba9-ef7a-4ecf-91b2-1e4ccb104ca6");
+            const res = await request(app).get(`/recipes/${invalidId}`);
             expect(res.statusCode).toBe(404);
-            expect(res.body.message).toBe("Recipe with id 71077ba9-ef7a-4ecf-91b2-1e4ccb104ca6 was not found");
+            expect(res.body.message).toBe(`Recipe with id ${invalidId} was not found`);
         });
     });
 
     describe("PATCH /recipes/:id", () => {
-        it("Should update a recipe successfully", async () => {
-            const res = await request(app).patch(`/recipes/${recipeId}`).send({
-                description: "A classic whiskey sour with a twist",
+        it ("Should not update a recipe without an authorization header", async () => {
+            const res = await request(app).patch(`/recipes/${recipe1Id}`).send({
+                description: "Description updated without auth",
             });
+            expect(res.statusCode).toBe(401);
+        });
+
+        it("Should not update a recipe that does not exist", async () => {
+            const res = await request(app).patch(`/recipes/${invalidId}`).send({
+                description: "Description updated by user1",
+            }).set('Authorization', `Bearer ${user1Token}`);
+            expect(res.statusCode).toBe(404);
+            expect(res.body.message).toBe(`Recipe with id ${invalidId} was not found`);
+        });
+
+        it("Should update a recipe as an admin user", async () => {
+            const res = await request(app).patch(`/recipes/${recipe1Id}`)
+                .send({
+                    description: "Description updated by admin",
+                })
+                .set('Authorization', `Bearer ${adminToken}`);
             expect(res.statusCode).toBe(200);
-            expect(res.body.description).toBe("A classic whiskey sour with a twist");
+            expect(res.body.description).toBe("Description updated by admin");
+        });
+
+        it("Should update a recipe as a valid user", async () => {
+            const res = await request(app).patch(`/recipes/${recipe1Id}`)
+                .send({
+                    description: "Description updated by user1",
+                })
+                .set('Authorization', `Bearer ${user1Token}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body.description).toBe("Description updated by user1");
+        });
+
+        it("Should not update a recipe as an invalid user", async () => {
+            const res = await request(app).patch(`/recipes/${recipe1Id}`)
+                .send({
+                    description: "Description updated by user2",
+                })
+                .set('Authorization', `Bearer ${user2Token}`);
+            expect(res.statusCode).toBe(403);
+            expect(res.body.message).toBe("You do not have permission to update this recipe");
         });
 
         it("Should update a recipe with an additional ingredient", async () => {
-            const res = await request(app).patch(`/recipes/${recipeId}`).send({
-                ingredients: [
-                    { id: honeySyrupId, quantity: 0.75, unit: "oz" },
-                    { id: lemonJuiceId, quantity: 1, unit: "oz" },
-                    { id: whiskeyId, quantity: 2, unit: "oz" },
-                    { id: bittersId, quantity: 1, unit: "dashes" },
-                ]
-            });
+            const res = await request(app).patch(`/recipes/${recipe1Id}`)
+                .send({
+                    ingredients: [
+                        { id: honeySyrupId, quantity: 0.75, unit: "oz" },
+                        { id: lemonJuiceId, quantity: 1, unit: "oz" },
+                        { id: whiskeyId, quantity: 2, unit: "oz" },
+                        { id: bittersId, quantity: 1, unit: "dashes" },
+                    ]
+                })
+                .set('Authorization', `Bearer ${user1Token}`);
             expect(res.statusCode).toBe(200);
             expect(res.body.ingredients.length).toBe(4);
-        });
-
-        it("Should not update a recipe with an invalid id", async () => {
-            const res = await request(app).patch("/recipes/71077ba9-ef7a-4ecf-91b2-1e4ccb104ca6").send({
-                description: "A classic whiskey sour with a twist",
-            });
-            expect(res.statusCode).toBe(404);
-            expect(res.body.message).toBe("Recipe with id 71077ba9-ef7a-4ecf-91b2-1e4ccb104ca6 was not found");
         });
     });
 
     describe("DELETE /recipes/:id", () => {
-        it("Should delete a recipe successfully", async () => {
-            const res = await request(app).delete(`/recipes/${recipeId}`);
+        it("Should not delete a recipe without an authorization header", async () => {
+            const res = await request(app).delete(`/recipes/${recipe1Id}`);
+            expect(res.statusCode).toBe(401);
+        });
+
+        it("Should not delete a recipe that does not exist", async () => {
+            const res = await request(app).delete(`/recipes/${invalidId}`)
+                .set('Authorization', `Bearer ${user1Token}`);
+            expect(res.statusCode).toBe(404);
+            expect(res.body.message).toBe(`Recipe with id ${invalidId} was not found`);
+        });
+
+        it("Should not delete a recipe as an invalid user", async () => {
+            const res = await request(app).delete(`/recipes/${recipe1Id}`)
+                .set('Authorization', `Bearer ${user2Token}`);
+            expect(res.statusCode).toBe(403);
+            expect(res.body.message).toBe("You do not have permission to delete this recipe");
+        });
+
+        it("Should delete a recipe as a valid user", async () => {
+            const res = await request(app).delete(`/recipes/${recipe1Id}`)
+                .set('Authorization', `Bearer ${user1Token}`);
             expect(res.statusCode).toBe(200);
             expect(res.body.message).toBe("Recipe deleted");
         });
 
-        it("Should not delete a recipe that was already deleted", async () => {
-            const res = await request(app).delete(`/recipes/${recipeId}`);
+        it("Should not delete a recipe that has already been deleted", async () => {
+            const res = await request(app).delete(`/recipes/${recipe1Id}`)
+                .set('Authorization', `Bearer ${user1Token}`);
             expect(res.statusCode).toBe(404);
-            expect(res.body.message).toBe(`Recipe with id ${recipeId} was not found`);
+            expect(res.body.message).toBe(`Recipe with id ${recipe1Id} was not found`);
         });
 
-        it("Should not delete a recipe with an invalid id", async () => {
-            const res = await request(app).delete("/recipes/71077ba9-ef7a-4ecf-91b2-1e4ccb104ca6");
-            expect(res.statusCode).toBe(404);
-            expect(res.body.message).toBe("Recipe with id 71077ba9-ef7a-4ecf-91b2-1e4ccb104ca6 was not found");
+        it("Should delete a recipe as an admin user", async () => {
+            const res = await request(app).delete(`/recipes/${recipe2Id}`)
+                .set('Authorization', `Bearer ${adminToken}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body.message).toBe("Recipe deleted");
         });
     });
 });
